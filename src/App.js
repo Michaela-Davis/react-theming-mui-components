6059577559dca8f6c61141057e0bb9e1ef76020e
Michaela Davis <michaela.delight@gmail.com>
2022-06-08 10:41:01 -0700

    add color design tokens

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
+import designTokens from './design-tokens.json'
 
 const theme = createTheme({
   palette: {
@@ -12,6 +12,16 @@ const theme = createTheme({
       light: '#28a99e',
       dark: '#025043',
       contrastText: '#fff',
+    },
+    secondary: {
+      main: designTokens.color.mainPurple,
+      light: designTokens.color.lightPurple,
+      dark: designTokens.color.darkPurple,
+    },
+    error: {
+      main: designTokens.color.primaryRed,
+      dark: designTokens.color.rocketRed,
+      contrastText: designTokens.color.primaryBlack,
     },
   },
 });
