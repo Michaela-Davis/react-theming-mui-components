7c97b87b747634b032c7e8f85a18ef54269d5603
Michaela Davis <michaela.delight@gmail.com>
2022-06-08 10:15:42 -0700

    :sparkles: init theming using Theme Provider/createTheme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#037c6e',
+      light: '#28a99e',
+      dark: '#025043',
+      contrastText: '#fff',
+    },
+  },
+});
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Michaela's React MUI Theming sample</h1>
+        </header>
+
+        <main>
+          <Card></Card>
+        </main>
+        
+        <footer>
+          <Button variant="contained" color="primary">
+            Main CTA (contained primary)
+          </Button>
+
+          
+        </footer>
+      </div>
+    </ThemeProvider>
   );
 }
 
